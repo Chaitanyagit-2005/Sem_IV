7109336a3d8a3cc0492d0b03211c06a8cf90867f
Chaitanyagit-2005 <2022bit041@sggs.ac.in>
2024-01-11 12:56:41 +0530

    Created class for engCalci and added first function to it.Code needs to be written neately without any syntax error

---
+++ Java/javaPracticals/Project/calci.java
@@ -0,0 +1,13 @@
+//Engineering calculator
+class engCalci{
+    public:
+      int a;
+      int b;
+      int add(int a, int b);
+};
+
+int engCalci::add(int a,int b){
+      return a+b;
+      
+      }
+      
